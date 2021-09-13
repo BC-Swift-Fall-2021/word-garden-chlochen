b685c853b8f4b6a1fe0f28c2dcb5363f661f5bb4
Chloe Chen <chloechen408@gmail.com>
2021-09-13 15:23:29 -0400

    3.3 complete, working keyboard, firstResponder, UITextField actions

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -10,32 +10,41 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var wordsGuessedLabel: UILabel!
-    
     @IBOutlet weak var wordsRemainingLabel: UILabel!
-    
     @IBOutlet weak var wordsMissedLabel: UILabel!
-    
     @IBOutlet weak var wordsInGame: UILabel!
-    
     @IBOutlet weak var wordBeingRevealedLabel: UILabel!
-    
     @IBOutlet weak var guessLetterTextField: UITextField!
-    
     @IBOutlet weak var guessLetterButton: UIButton!
-    
     @IBOutlet weak var playAgainButton: UIButton!
-    
     @IBOutlet weak var gameStatusMessageLabel: UILabel!
-    
     @IBOutlet weak var flowerImageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let text = guessLetterTextField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
     }
-
+    
+    func updateUIAfterGuess () {
+        guessLetterTextField.resignFirstResponder()
+        guessLetterTextField.text! = ""
+        guessLetterButton.isEnabled = false
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        let text = guessLetterTextField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
+        
+    }
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        
+    }
+    
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
+        
     }
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
     }
